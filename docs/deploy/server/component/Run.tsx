36644fcad40b3e703278fd8d1c9ee1c701401eb2
Marvin <yongfeide123@gmail.com>
2025-01-01 23:32:04 +0800

    feat: code

---
+++ docs/deploy/server/component/Run.tsx
@@ -0,0 +1,5 @@
+// @ts-ignore
+import { PackageManagerTabs } from '@theme';
+export default (props:{ command: string}) => {
+  return <PackageManagerTabs command={props.command} />
+}
---
+++ docs/deploy/web/component/Build.tsx
@@ -0,0 +1,5 @@
+// @ts-ignore
+import { PackageManagerTabs } from '@theme';
+export default () => {
+  return <PackageManagerTabs command="run build:min" />
+}
